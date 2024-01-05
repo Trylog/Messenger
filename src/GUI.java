e884bd687476ba141f9a1cda9304c59a1a37afba
Protetor074 <101059740+Protetor074@users.noreply.github.com>
2024-01-05 19:18:50 +0100

    Okno dolacania do czatu

--- src/GUI.java
+++ src/GUI.java
@@ -138,7 +138,7 @@ public class GUI {
 		public static JScrollPane newMessagePane;
 		public static int respondingId = -1;
 		private static String curentConversationName;
-		 public static Image icon;
+		public static Image icon;
 
 		MainGUI(){
 
@@ -275,16 +275,14 @@ public class GUI {
 			if(e.getSource() == createChatButtonMenu){
 				//Wyświetlenie Panelu Moderatora
 				System.out.println("PStwórz czat");
-
+				CreateChatGUI createChatGUI = new CreateChatGUI();
 			}
 			if(e.getSource() == joinChatButtonButtonMenu){
 				//Wyświetlenie Panelu Moderatora
 				System.out.println("Dołącz do konwersacji");
-
+				JoinChatGUI joinChatGUI = new JoinChatGUI();
 			}
 		}
-
-
 	}
 
 	private static class MessagesListElement extends JPanel{
@@ -727,10 +725,6 @@ public class GUI {
 			removeUserFormChatButton.addActionListener(this);
 			this.add(removeUserFormChatButton);
 
-
-
-
-
 		}
 		private void performSearchUsers() {
 			String searchPhrase = urserSearchField.getText().toLowerCase();
@@ -864,14 +858,17 @@ public class GUI {
 		private JButton downUserPermissions;
 		private JButton upUserPermissions;
 
+		private String chatName;
+
 		ModeratorPanelFrame(String name){
+			chatName = name;
 
 			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 			this.setResizable(false);
 			this.setLocationRelativeTo(null);
 			this.setLayout(null);
 			this.setSize(940, 680);
-			this.setTitle("Messenger - Panel Moderatora - " + name);
+			this.setTitle("Messenger - Panel Moderatora - " + chatName);
 			Image icon = new ImageIcon("src/textures/appIcon.png").getImage();
 			this.setIconImage(icon);
 			this.setVisible(true);
@@ -1134,6 +1131,7 @@ public class GUI {
 			}
 			if(e.getSource() == editChatDataButton){
 				System.out.println("Edycja danych czatu");
+				ChatDataModyfieGUI chatDataModyfieGUI = new ChatDataModyfieGUI(chatName);
 			}
 			if(e.getSource() == removeConversationButton){
 				System.out.println("Konwersacja usunięta");
@@ -1190,6 +1188,206 @@ public class GUI {
 			button.setBounds(x,y,width,height);
 			button.addActionListener(this);
 			this.add(button);
+		}
+	}
+
+	private static class ChatDataModyfieGUI extends JFrame implements ActionListener {
+		ChatDataModyfieGUI(String chatName) {
+			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			this.setResizable(false);
+			this.setLocationRelativeTo(null);
+			this.setLayout(null);
+			this.setSize(640, 640);
+			this.setTitle("Modyfikacja danych czatu - " + chatName);
+			Image icon = new ImageIcon("src/textures/appIcon.png").getImage();
+			this.setIconImage(icon);
+			this.setVisible(true);
+
+			int border = 10;
+
+			//TODO Możliwość zmiany Nazwy czatu + możliwość zmiany awatara czatu
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			/*if(e.getSource() == addUserToConversationButton){
+				System.out.println("Dodanie użytkownika");
+			}*/
+		}
+	}
+
+	private static class CreateChatGUI extends JFrame implements ActionListener {
+		CreateChatGUI() {
+			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			this.setResizable(false);
+			this.setLocationRelativeTo(null);
+			this.setLayout(null);
+			this.setSize(640, 640);
+			this.setTitle("Okno tworzenia czatu");
+			Image icon = new ImageIcon("src/textures/appIcon.png").getImage();
+			this.setIconImage(icon);
+			this.setVisible(true);
+
+			int border = 10;
+
+			//TODO Możliwość ustawienia Nazwy czatu + możliwość ustawienia avatara awatara czatu + początkowe dodanie członków + moderatorów
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			/*if(e.getSource() == addUserToConversationButton){
+				System.out.println("Dodanie użytkownika");
+			}*/
+		}
+	}
+
+	private static class JoinChatGUI extends JFrame implements ActionListener {
+		private JScrollPane chatScrollPane;
+		private JPanel chatListPanel;
+		private JTextField chatSearchField;
+		private ButtonGroup chatButtonGroup;
+		private JPanel chatInfoTextPanel;
+		private JTextArea chatInfoTextArea;
+		private JButton joinSelectedChat;
+		JoinChatGUI() {
+			this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			this.setResizable(false);
+			this.setLocationRelativeTo(null);
+			this.setLayout(null);
+			this.setSize(460, 380);
+			this.setTitle("Okno Dołaczania do czatu");
+			Image icon = new ImageIcon("src/textures/appIcon.png").getImage();
+			this.setIconImage(icon);
+			this.setVisible(true);
+
+			int border = 10;
+
+			// Panel z przewijaniem dla czatu
+			chatScrollPane = new JScrollPane();
+			chatScrollPane.setBorder(BorderFactory.createTitledBorder("Lista Czatów"));
+			chatScrollPane.setBounds(border, border, 420, 170);
+			chatScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+			// Panel z komponentami czatu (dodaj swoje komponenty)
+			chatListPanel = new JPanel();
+			chatListPanel.setLayout(new BoxLayout(chatListPanel, BoxLayout.Y_AXIS));
+			chatScrollPane.setViewportView(chatListPanel);
+
+			// Grupa dla przycisków czatów
+			chatButtonGroup = new ButtonGroup();
+
+			// Przykładowe dodanie kilku czatów
+			for (int i = 0; i < 20; i++) {
+				JToggleButton chatButton = new JToggleButton("Czat " + i);
+				chatButton.setAlignmentX(Component.LEFT_ALIGNMENT); // Ustawienie przycisku na lewo
+				Dimension buttonSize = new Dimension(390, 30);
+				chatButton.setPreferredSize(buttonSize);
+				chatButton.setMaximumSize(buttonSize);
+				chatButton.addActionListener(new ChatButtonListener());
+				chatListPanel.add(chatButton);
+				chatButtonGroup.add(chatButton);
+			}
+
+			// Panel z wyszukiwaniem czatów
+			JPanel chatSearchPanel = new JPanel();
+			chatSearchPanel.setBorder(BorderFactory.createTitledBorder("Wyszukaj Czat"));
+			chatSearchPanel.setBounds(border, 170 + border, 420, 60);
+
+			// Pole tekstowe do wprowadzania frazy wyszukiwania czatów
+			chatSearchField = new JTextField();
+			chatSearchField.setPreferredSize(new Dimension(400, 25));
+			chatSearchField.getDocument().addDocumentListener(new DocumentListener() {
+				@Override
+				public void insertUpdate(DocumentEvent e) {
+					performSearchChats();
+				}
+				@Override
+				public void removeUpdate(DocumentEvent e) {
+					performSearchChats();
+				}
+				@Override
+				public void changedUpdate(DocumentEvent e) {
+					performSearchChats();
+				}
+			});
+			chatSearchPanel.add(chatSearchField);
+
+			// Dodanie komponentów do ramki
+			this.add(chatScrollPane);
+			this.add(chatSearchPanel);
+
+			// Pole tekstowe z informacjami o wybranym czacie
+			chatInfoTextArea = new JTextArea();
+			chatInfoTextPanel = createInfoTextArea(border, border + 230, "Wybrany czat", 420, 60, chatInfoTextArea);
+
+			//Przyciski Aktywnosci
+
+			joinSelectedChat = new JButton("Dołącz do czatu");
+			joinSelectedChat.setBounds(border,290+ border,420,30);
+			joinSelectedChat.addActionListener(this);
+			this.add(joinSelectedChat);
+		}
+		private void performSearchChats() {
+			String searchPhrase = chatSearchField.getText().toLowerCase();
+			Component[] components = chatListPanel.getComponents();
+
+			for (Component component : components) {
+				if (component instanceof JToggleButton) {
+					JToggleButton chatButton = (JToggleButton) component;
+					String buttonText = chatButton.getText().toLowerCase();
+					chatButton.setVisible(buttonText.contains(searchPhrase));
+				}
+			}
+			chatListPanel.revalidate();
+			chatListPanel.repaint();
+		}
+		public void addChatToList(String chatName) {
+			JToggleButton chatButton = new JToggleButton(chatName);
+			chatButton.setAlignmentX(Component.LEFT_ALIGNMENT); // Ustawienie przycisku na lewo
+			Dimension buttonSize = new Dimension(390, 30);
+			chatButton.setPreferredSize(buttonSize);
+			chatButton.setMaximumSize(buttonSize);
+			chatButton.addActionListener(new ChatButtonListener());
+			chatListPanel.add(chatButton);
+			chatButtonGroup.add(chatButton);
+
+			chatListPanel.revalidate();
+			chatListPanel.repaint();
+		}
+		private JPanel createInfoTextArea(int x, int y, String title, int width, int height, JTextArea infoTextArea ) {
+			JPanel infoTextPanel = new JPanel();
+
+			infoTextPanel.setBorder(BorderFactory.createTitledBorder(title));
+			infoTextPanel.setBounds(x, y, width, height);
+
+			infoTextArea.setEditable(false);
+
+			Font newFont = new Font("Arial", Font.BOLD, 12); // Zmień 16 na dowolny rozmiar czcionki
+			Dimension dimension = new Dimension(width-20, height-40);
+			infoTextArea.setPreferredSize(dimension);
+			infoTextArea.setFont(newFont);
+
+			infoTextPanel.add(infoTextArea);
+			this.add(infoTextPanel);
+
+			return infoTextPanel;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == joinSelectedChat){
+				if(chatInfoTextArea.getText().equals("")){
+					JOptionPane.showMessageDialog(null, "Nie wybrano chatu", "Błąd", JOptionPane.ERROR_MESSAGE);
+				}else{
+					System.out.println("Dołączono do czatu - " + chatInfoTextArea.getText());
+				}
+			}
+		}
+		private class ChatButtonListener implements ActionListener {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Możesz dodać tu kod obsługi zdarzenia dla wybranego użytkownika
+				// W tej chwili zaznaczony przycisk można sprawdzić używając:
+				JToggleButton chatButton = (JToggleButton) e.getSource();
+				chatInfoTextArea.setText(chatButton.getText());
+			}
 		}
 	}
 }
